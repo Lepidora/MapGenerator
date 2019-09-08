884ddce0d95a21f97fe342ca97f4e2c87478fc41
Lepidora <miamukherjee2+lepidora@gmail.com>
2019-09-08 17:55:37 +0100

    Added body parsing and stopped get_path taking ownership

--- src/parser.rs
+++ src/parser.rs
@@ -5,10 +5,11 @@
  */
 
 use httparse;
+use json;
 
 use std::iter::FromIterator;
 
- pub fn get_path(header: String) -> Option<String> {
+ pub fn get_path(header: &String) -> Option<String> {
  
 	let mut headers = [httparse::EMPTY_HEADER; 16];
 
@@ -33,4 +34,28 @@ use std::iter::FromIterator;
 	split_url.retain(|s| s.len() > 0);
 
 	return split_url;
+ }
+
+ pub fn get_body(request: &String) -> Option<String> {
+	
+	let split_request = Vec::from_iter(request.splitn(2, "\r\n\r\n").map(String::from));
+
+	if split_request.len() > 1 {
+		return Some(split_request[1].to_string());
+	} else {
+		return None;
+	}
+ }
+
+ pub fn parse_body(body_string: Option<String>) -> json::Result<json::JsonValue> {
+
+	match body_string {
+		Some(contents) => {
+		
+			let trimmed = contents.trim_matches(char::from(0)).trim();
+			
+			json::parse(&trimmed)
+		},
+		None => json::parse("")
+	}
  }
