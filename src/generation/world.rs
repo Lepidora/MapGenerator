9819448a2537bd1360e7c359cbb9ce35ae67f68d
Lepidora <miamukherjee2+lepidora@gmail.com>
2019-09-08 17:54:19 +0100

    Refactored world generation and added methods to print world data

--- src/generation/world.rs
+++ src/generation/world.rs
@@ -4,34 +4,43 @@
  *  Handles generation of high level world details like continents and climates at world level 0
  */
 
-extern crate rand;
-
-use rand::Rng;
-
- pub struct World {
+#[derive(Clone)]
+pub struct World {
 	pub name: String,
 	pub id: u64,
 	pub seed: String,
 	pub sea_level: f64,
 	pub temperature: f64,
 	pub humidity: f64
- }
+	
+}
 
- impl World {
+impl World {
  
-	fn to_string(&self) -> String {
+	pub fn to_string(&self) -> String {
 		
 		let representation = format!("ID: {} Name: {} Seed: {} Sea Level: {} Temperature: {} Humidity: {}", self.id, self.name, self.seed, self.sea_level, self.temperature, self.humidity);
 
 		return representation;
 	}
- }
 
- pub fn new_world() -> u64 {
-	
-	let mut rng = rand::thread_rng();
+	pub fn to_json(&self) -> String {
+		
+		let representation = format!("{{\"name\":\"{}\",\"id\":\"{}\",\"seed\":\"{}\",\"sea_level\":\"{}\",\"temperature\":\"{}\",\"humidity\":\"{}\"}}", self.name, self.id, self.seed, self.sea_level, self.temperature, self.humidity);
 
-	let id: u64 = rng.gen();
+		return representation;
+	}
+}
 
-	return id;
- }
+impl Default for World {
+	fn default() -> World {
+		World {
+			name: "".to_string(),
+			id: 0,
+			seed: "".to_string(),
+			sea_level: 0.0,
+			temperature: 0.0,
+			humidity: 0.0
+		}
+	}
+}
